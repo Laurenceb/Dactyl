44cecee0f31cce65184607742cdf8536b0d19fa0
Laurence Blaxter <eexlb2@nottingham.ac.uk>
2011-09-29 17:39:55 +0100

    pitot mask word

--- Sensors/pitot.c
+++ Sensors/pitot.c
@@ -7,6 +7,27 @@
 #include "../Util/atmospherics.h"
 
 /**
+  * @brief  Reads the raw adc data and returns 32 bit signed integer value (17 usable bits)
+  * @param  Uncorrected value
+  * @retval Corrected value
+  */
+int32_t Pitot_Conv(uint32_t d) {
+	d&=0x00FFFFFF;
+	if(d&0x00400000)return ((d>>6)|0xFFFF0000);//-ive result
+	else return ((d>>6)&0x0000FFFF);	//+ive result
+}
+
+/**
+  * @brief  Reads the corrected adc data and returns float with airspeed TODO(This reallspeedy needs temperature compensation)
+  * @param  Uncorrected value (int32_t), altitude in meters (float), barometric pressure (float)
+  * @retval Corrected value ms^-1 (float)
+  */
+float Pitot_convert_Airspeed(int32_t P,float Altitude, float Baro_pressure) {
+	return sqrt(fabs(((float)(P-PITOT_OFFSET))*PITOT_GAIN*2.0/Air_Density(Altitude,Baro_pressure)));
+}
+
+#ifdef PITOT_POLLED
+/**
   * @brief  Sets up a pressure conversion
   * @param  None
   * @retval Success code
@@ -38,22 +59,4 @@ I2C_Returntype Pitot_Read_Conv(uint32_t* adc) {
 	*adc=(((uint32_t)b[0] << 16) | ((uint32_t)b[1] <<8) | ((uint16_t)b[2])) & 0x00FFFFFF;
 	return t;
 }
-
-/**
-  * @brief  Reads the raw adc data and returns 32 bit signed integer value (17 usable bits)
-  * @param  Uncorrected value
-  * @retval Corrected value
-  */
-int32_t Pitot_Conv(uint32_t d) {
-	if(d&0x00400000)return ((d>>6)|0xFFFF0000);//-ive result
-	else return ((d>>6)&0x0000FFFF);	//+ive result
-}
-
-/**
-  * @brief  Reads the corrected adc data and returns float with airspeed TODO(This reallspeedy needs temperature compensation)
-  * @param  Uncorrected value (int32_t), altitude in meters (float), barometric pressure (float)
-  * @retval Corrected value ms^-1 (float)
-  */
-float Pitot_convert_Airspeed(int32_t P,float Altitude, float Baro_pressure) {
-	return sqrt(fabs(((float)(P-PITOT_OFFSET))*PITOT_GAIN*2.0/Air_Density(Altitude,Baro_pressure)));
-}
+#endif
