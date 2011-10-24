ff04cb3f8c99056c53b13aa9c1f462ba52aaead1
Laurence Blaxter <eexlb2@nottingham.ac.uk>
2011-10-24 14:20:53 +0100

    Error isr fix

--- interrupts.c
+++ interrupts.c
@@ -78,7 +78,7 @@ void EXTI_Config(void)
   //NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
   //Now we configure the I2C Error ISR
-  NVIC_InitStructure.NVIC_IRQChannel = I2C1_EV_IRQn;//The I2C1 triggered interrupt	
+  NVIC_InitStructure.NVIC_IRQChannel = I2C1_ER_IRQn;//The I2C1 triggered interrupt	
   //NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;//Higher pre-emption priority
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;//Highest group priority
   //NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
