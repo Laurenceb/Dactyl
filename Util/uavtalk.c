68cc25f0364223c3f8ceb24f2a3f216c6fc59755
Laurence Blaxter <Laurence.blaxter@btinternet.com>
2011-11-11 21:12:08 +0000

    UAVtalk- always send Flight_status

--- Util/uavtalk.c
+++ Util/uavtalk.c
@@ -190,23 +190,25 @@ void UAVtalk_Run_Streams(UAVtalk_Port_Type* port,Buffer_Type* buff,uint32_t upti
 	uint8_t packet_gen=0;			//Logical to let us know a packet has been generated
 	static uint32_t millis=0;		//Local time variable
 	port->type=0x00;			//We only stream basic objects
-	for(i=0;i<UAVtalk_conf.num_stream_objects && port->flightStats.Status;i++) {//Connected or Handshake ACK? send objects
-		UAVtalk_conf.stream_timers[i]-=(uptime-millis);//Adjust timer
-		if(UAVtalk_conf.stream_timers[i]<0 && !packet_gen) {//Timer expired
-			//Stop if the buffer is full - try and fill it as full as poss, note tail holds the top of data
-			if((UAVtalk_conf.lenghts[UAVtalk_conf.stream_object_nos[i]]+11+buff->tail)<buff->size){
-				port->object_no=UAVtalk_conf.stream_object_nos[i];//Set the obj no (i.e. 0,1,2,3,4 as in objectid array)
-				//Note we do not set the instance here (TODO find out if its essential)
-				//UAVtalk_conf.semaphores[port->object_no]=WRITE;//Set the object as written (Note done externally)
-				port->type=UAVtalk_conf.stream_object_types[i];//Set the type
-				UAVtalk_Generate_Packet(port,buff);//Generate the expired packet
-				UAVtalk_conf.stream_timers[i]=UAVtalk_conf.stream_intervals[i];//Reset to default
-				//Check to see if the next packet will go past tryfor limit, and if so give up
-				if((UAVtalk_conf.lenghts[UAVtalk_conf.stream_object_nos[i]]+11+buff->tail)>tryfor && tryfor)
+	for(i=0;i<UAVtalk_conf.num_stream_objects;i++) {//Loop through, sending objects
+		if(port->flightStats.Status||UAVtalk_conf.stream_object_nos[i]==FLIGHT_STATS) {//only process if connected, except for flight stats
+			UAVtalk_conf.stream_timers[i]-=(uptime-millis);//Adjust timer
+			if(UAVtalk_conf.stream_timers[i]<0 && !packet_gen) {//Timer expired
+				//Stop if the buffer is full - try and fill it as full as poss, note tail holds the top of data
+				if((UAVtalk_conf.lenghts[UAVtalk_conf.stream_object_nos[i]]+11+buff->tail)<buff->size){
+					port->object_no=UAVtalk_conf.stream_object_nos[i];//Set the obj no (i.e. 0,1,2,3,4 as in objectid array)
+					//Note we do not set the instance here (TODO find out if its essential)
+					//UAVtalk_conf.semaphores[port->object_no]=WRITE;//Set the object as written (Note done externally)
+					port->type=UAVtalk_conf.stream_object_types[i];//Set the type
+					UAVtalk_Generate_Packet(port,buff);//Generate the expired packet
+					UAVtalk_conf.stream_timers[i]=UAVtalk_conf.stream_intervals[i];//Reset to default
+					//Check to see if the next packet will go past tryfor limit, and if so give up
+					if((UAVtalk_conf.lenghts[UAVtalk_conf.stream_object_nos[i]]+11+buff->tail)>tryfor && tryfor)
+						packet_gen=1;
+				}
+				else
 					packet_gen=1;
 			}
-			else
-				packet_gen=1;
 		}
 	}
 	millis=uptime;				//Set our local time variable
